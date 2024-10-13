f6a260c6025812b035e6482bdae81a6b548ccac5
Arifin-MRX <arifinmrx@gmail.com>
2024-10-14 00:19:33 +0700

    mengubah id produc ke nama produc dan juga material

--- app/Http/Controllers/Api/BomController.php
+++ app/Http/Controllers/Api/BomController.php
@@ -13,38 +13,38 @@ class BomController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-{
-    $boms = Bom::with(['product', 'bom_components.material'])->get(); 
-    $data = $boms->map(function ($bom) {
-        $bom_components = $bom->bom_components->map(function ($component) {
-            $material_cost = $component->material->cost;
-            $material_total_cost = $material_cost * $component->material_qty;
+    {
+        $boms = Bom::with(['product', 'bom_components.material'])->get();
+        $data = $boms->map(function ($bom) {
+            $bom_components = $bom->bom_components->map(function ($component) {
+                $material_cost = $component->material->cost;
+                $material_total_cost = $material_cost * $component->material_qty;
+                return [
+                    'material_name' => $component->material->material_name,
+                    'material_qty' => $component->material_qty,
+                    'material_cost' => $material_cost,
+                    'material_total_cost' => $material_total_cost,
+                ];
+            });
+            $bom_cost = $bom_components->sum('material_total_cost');
+
             return [
-                'material_id' => $component->material_id,
-                'material_qty' => $component->material_qty,
-                'material_cost' => $material_cost, 
-                'material_total_cost' => $material_total_cost,
+                'bom_id' => $bom->bom_id,
+                'product_name' => $bom->product->product_name,
+                'product_qty' => $bom->product_qty,
+                'product_cost' => $bom->product->cost,
+                'bom_components' => $bom_components,
+                'bom_cost' => $bom_cost,
             ];
         });
-        $bom_cost = $bom_components->sum('material_total_cost');
 
-        return [
-            'bom_id' => $bom->bom_id,
-            'product_id' => $bom->product_id,
-            'product_qty' => $bom->product_qty,
-            'product_cost' => $bom->product->cost, 
-            'bom_components' => $bom_components,
-            'bom_cost' => $bom_cost, 
-        ];
-    });
-
-    // Return data yang sudah diformat
-    return response()->json([
-        'success'   => true,
-        'message'   => 'Data BOM berhasil diambil',
-        'data'      => $data
-    ], 200);
-}
+        // Return data yang sudah diformat
+        return response()->json([
+            'success'   => true,
+            'message'   => 'Data BOM berhasil diambil',
+            'data'      => $data
+        ], 200);
+    }
 
 
     /**
